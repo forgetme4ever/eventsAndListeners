16089c924cbcf4a4c8bfba0304828874a325b4d5
Eugen Burkenia <burikievgen@gmail.com>
2021-12-16 23:47:18 -0500

    added % sign and default value

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const text = document.getElementById('text');
 const circle = document.getElementById('circle');
 const btnInCircle = document.getElementById('e_btn');
 const slider = document.getElementById('range');
-const span = document.getElementById('range-span')
+let span = document.getElementById('range-span')
+
+span.textContent = slider.value + '%';
 
 const colorChange = (event) => {
     let color = event.target.value;
@@ -12,16 +14,14 @@ const colorChange = (event) => {
     console.log(color);
 };
 
-
 const radiusChange = (event) => {
     
-    span.textContent = event.target.value;
+    span.textContent = event.target.value + '%';
     circle.style.width = event.target.value + '%';
     circle.style.height = event.target.value + '%';
 };
 
 btn.addEventListener('click', text.addEventListener('change', colorChange));
-
 
 btnInCircle.style.display = 'none';
 
